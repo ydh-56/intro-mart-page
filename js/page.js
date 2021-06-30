9bec37f2e11120e1449c11d01f04804f1d5ab56d
yangdahyun <govl1535@naver.com>
2021-06-30 22:38:56 +0900

    첫번째 커밋

---
+++ js/page.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('.visual .slide').slick({
+       arrows: false, 
+       dots: false, 
+       fade: true,
+       autoplay:true, 
+       autoplaySpeed: 5000, 
+       pauseOnHover: false,
+       pauseOnFocus: false   
+   });
+     
+});
